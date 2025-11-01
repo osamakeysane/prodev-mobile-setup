3036e91dc61d6cd987f9a882967b1004768d730f
mohamed keytsane <osamakeysane20@gmail.com>
2025-11-01 15:25:37 +0300

    .

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,47 +1,57 @@
 import { styles } from "@/styles";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"; // ORDER FIXED
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
+        {/* NAVGROUP */}
         <View style={styles.navGroup}>
           <Ionicons name="arrow-back" size={25} />
           <Image source={require("@/assets/images/logo.png")} />
         </View>
+
+        {/* TEXT GROUP */}
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
         <Text style={styles.smallText}>
           Enter your email and password to sign in.
         </Text>
 
+        {/* FORM GROUP */}
         <View style={styles.formGroup}>
+          {/* Email Input */}
           <View>
             <Text style={styles.placeholderText}>Email</Text>
             <TextInput keyboardType="email-address" style={styles.inputField} />
           </View>
+
+          {/* Password Input */}
           <View style={{ marginTop: 20 }}>
             <Text style={styles.placeholderText}>Password</Text>
             <View style={styles.passwordGroup}>
-              <TextInput style={{ flex: 1 }} />
+              <TextInput style={{ flex: 1 }} secureTextEntry />
               <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
             </View>
+            <Text style={styles.forgotPasswordText}>Forgot password?</Text>
           </View>
-          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
         </View>
 
+        {/* SIGN IN BUTTON */}
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Sign in</Text>
         </TouchableOpacity>
 
+        {/* DIVIDER */}
         <View style={styles.dividerGroup}>
-          <View style={styles.divider}></View>
+          <View style={styles.divider} />
           <Text style={styles.dividerText}>OR</Text>
-          <View style={styles.divider}></View>
+          <View style={styles.divider} />
         </View>
 
+        {/* SOCIAL MEDIA SIGN IN BUTTONS */}
         <View style={styles.socialMediaButtonGroup}>
           <TouchableOpacity style={styles.socialMediaButton}>
             <View
@@ -74,6 +84,7 @@ export default function Index() {
           </TouchableOpacity>
         </View>
 
+        {/* SUBTEXT */}
         <View style={styles.subTextGroup}>
           <Text style={styles.subText}>Don't have an account?</Text>
           <Text style={styles.subTextJoin}>Join now</Text>
